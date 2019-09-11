41fb247633447a1f73bf85ae3bab752fd8551a16
david <d.marsalone@gmail.com>
2019-09-11 06:32:26 +0200

    CayceoUi.js pour une version minimale à charger

---
+++ src/CayceoUi.js
@@ -0,0 +1,16 @@
+import Casque from "./ui/components/Casque";
+import Home from "./ui/screens/Home";
+import Dashboard from "./ui/popin/Dashboard";
+import Filters from "./ui/screens/Filters";
+import Films from "./ui/screens/Films";
+import Validation from "./ui/screens/Validation";
+import Explication from "./ui/screens/Explication";
+import Layout from "./ui/layout/Layout";
+import SelectCasques from "./ui/screens/SelectCasques";
+import ScreenUi from "./ui/screens/ScreenUi";
+import CasqueList from "./ui/components/CasqueList";
+import Ui from "./ui/Ui";
+
+window.$=document.$ = window.jQuery = require("jquery");
+window.$body=$("body");
+window.ui=new Ui();
