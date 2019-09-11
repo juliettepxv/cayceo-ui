08e6381483c33103cc5c191b9cb10ec84cdd5087
david <d.marsalone@gmail.com>
2019-09-11 19:02:46 +0200

    les logs font aussi un console.log maintenant

--- src/ui/popin/Logs.js
+++ src/ui/popin/Logs.js
@@ -1,21 +1,24 @@
 import PopinUi from "./PopinUi"
-import Ui from "../Ui";
 
+/**
+ * Contenu de popin permettant d'afficher des logs
+ */
 export default class Logs extends PopinUi{
-    /**
-     *
-     */
     constructor(){
         super();
-        let me=this;
         this.logs="";
         this.$main=$(require("./logs.html"));
     }
 
+    /**
+     * Enregistre une ligne de log
+     * @param {string} str Ce qu'il faut logger :)
+     */
     log(str){
         str=new Date().toUTCString()+"\n"+str;
         this.logs=str+"\n\n"+this.logs;
         this.$main.find("textarea").val(this.logs);
+        console.log(str);
     }
 
 }
