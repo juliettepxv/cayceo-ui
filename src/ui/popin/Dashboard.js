7442becb79d61265521670eb3fc082021508cffb
david <d.marsalone@gmail.com>
2019-10-23 14:54:24 +0200

    1.3.6 réveiller les casques sur le dasboard

--- src/ui/popin/Dashboard.js
+++ src/ui/popin/Dashboard.js
@@ -9,6 +9,7 @@ export default class Dashboard extends PopinUi{
         this.$main=$(require("./dashboard.html"));
         this.on(Ui.EVENT_ADDED_TO_STAGE,function(){
             me.$main.find("#casques").append(CasqueList.inst().$main);
+            ui.emit(CMD.WAKE_UP_CASQUES);
         });
     }
 
