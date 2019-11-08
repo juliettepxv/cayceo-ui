7d4a5cee1efda0bfb79aef3d5cf0ea2a1be4a848
david <d.marsalone@gmail.com>
2019-11-08 09:59:59 +0100

    maj webpack (terser es6)

--- _builders/webpack.prod.js
+++ _builders/webpack.prod.js
@@ -1,18 +1,33 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin')
 const common = require('./webpack.common.js');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 var mode="production";
 module.exports = merge(common, {
     devtool: 'source-map',
     "mode": mode,
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin(
+                {
+                    terserOptions:{
+                        ecma: 6,
+                    }
+                }
+            ),
+            new OptimizeCSSAssetsPlugin({})
+        ],
+    },
     plugins: [
-        new UglifyJSPlugin({sourceMap: true}),
+        //new UglifyJSPlugin({sourceMap: true}),
         new webpack.DefinePlugin({
             NODE_ENV: JSON.stringify(mode),
             PRODUCTION: JSON.stringify(true)
         })
-   ],
+    ],
     module:{
         rules:[
             {
